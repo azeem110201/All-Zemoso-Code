52e815684a7eecef12caf2caad74f90b66297a6a
azeem <mohd.abazeem@gmail.com>
2022-02-02 15:37:09 +0530

    added assignment 8 exception handling and added problem desc

--- Java Assignments/Assignment_8/src/com/azeem/Main.java
+++ Java Assignments/Assignment_8/src/com/azeem/Main.java
@@ -2,6 +2,14 @@ package com.azeem;
 import java.util.Scanner;
 
 public class Main {
+    /*
+       Problem Description
+
+       Create three new types of exceptions.
+       Write a class with a method that throws all three.
+       In main( ), call the method but only use a single catch clause that will catch all three types of exceptions.
+       Add a finally clause and verify that your finally clause is executed, even if a NullPointerException is thrown.
+     */
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
 
