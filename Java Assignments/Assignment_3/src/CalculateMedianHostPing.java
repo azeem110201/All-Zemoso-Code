e7d52fcc5182f31e569def41fe1872c523d5bc58
azeem110201 <mohd.abazeem@gmail.com>
2022-02-09 09:14:44 +0530

    Added host ping assignment 3

---
+++ Java Assignments/Assignment_3/src/CalculateMedianHostPing.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class CalculateMedianHostPing {
+    public static void getMedianHostPingTime(String HostIPAddress) throws IOException {
+        long start=System.currentTimeMillis();
+
+        InetAddress giveAddress=InetAddress.getByName(HostIPAddress);
+
+        System.out.println("Canonical host name for the given IP Address:" + giveAddress.getCanonicalHostName());
+        System.out.println("Get the host name for the given IP Address:" + giveAddress.getHostAddress());
+
+        System.out.println("Sending Ping request to host "+ HostIPAddress +".......");
+        if(giveAddress.isReachable(10000)){
+            System.out.println("Host is reachable.");
+        }
+        else{
+            System.out.println("Host is not reachable. Please try again.");
+            return;
+        }
+        long end=System.currentTimeMillis();
+        long time=(end-start)>>1;
+
+        System.out.println("Median time in milliseconds:" + time);
+    }
+    public static void main(String[] args) throws IOException {
+        System.out.println("For LocalHost............");
+        getMedianHostPingTime("127.0.0.1");
+
+        System.out.println("For Google............");
+        getMedianHostPingTime("google.com");
+
+        System.out.println("For Zemoso............");
+        getMedianHostPingTime("zemosolabs.com");
+    }
+}
